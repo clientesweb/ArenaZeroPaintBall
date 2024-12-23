6c9d13685bad5e97c86cad7d2d61a2622052bed4
clientesweb <alexisifran32@gmail.com>
2024-12-22 21:14:53 -0300

    Add files via upload

---
+++ utils/constants.ts
@@ -0,0 +1,12 @@
+export const WHATSAPP_NUMBER = "543546545506"
+export const WHATSAPP_MESSAGE = "¡Hola! Me gustaría hacer una reserva en Arena Zero Paintball"
+
+export const theme = {
+  colors: {
+    primary: "#006400",
+    secondary: "#D4C8B0",
+    accent: "#FFFFFF",
+    background: "#F5F5DC"
+  }
+}
+
---
+++ utils/fonts.ts
@@ -0,0 +1,7 @@
+import { Black_Ops_One } from 'next/font/google'
+
+export const militaryFont = Black_Ops_One({ 
+  weight: '400',
+  subsets: ['latin'],
+})
+
