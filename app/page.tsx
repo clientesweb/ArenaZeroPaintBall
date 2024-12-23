b4581d86b2cf9a35244a926405dfdbe4ec00199c
clientesweb <alexisifran32@gmail.com>
2024-12-22 23:44:41 -0300

    Actualizar page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,20 @@
-import { Navigation } from '../components/navigation'
 import { Hero } from '../components/hero'
 import { Services } from '../components/services'
 import { Gallery } from '../components/gallery'
 import { InstagramReels } from '../components/instagram-reels'
 import { Pricing } from '../components/pricing'
 import { Testimonials } from '../components/testimonials'
-import { WhatsAppButton } from '../components/whatsapp-button'
-import { Footer } from '../components/footer'
-import { Preloader } from '../components/preloader'
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Preloader />
-      <Navigation />
-      <main className="flex-grow">
-        <Hero />
-        <Services />
-        <Gallery />
-        <InstagramReels />
-        <Pricing />
-        <Testimonials />
-      </main>
-      <WhatsAppButton />
-      <Footer />
-    </div>
+    <>
+      <Hero />
+      <Services />
+      <Gallery />
+      <InstagramReels />
+      <Pricing />
+      <Testimonials />
+    </>
   )
 }
 
